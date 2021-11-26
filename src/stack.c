2608fad2c3849249322ee5fa093669da6c29f096
Paul Philippov <themactep@gmail.com>
2021-11-26 11:32:08 -0500

    Use weak linking to let it compile with Mips toolchain.

--- src/stack.c
+++ src/stack.c
@@ -7,9 +7,10 @@
 #define STACK_CHK_GUARD 0x595e9fbd94fda766
 #endif
 
+__attribute__((weak))
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-__attribute__((noreturn))
+__attribute__((weak, noreturn))
 void __stack_chk_fail(void)
 {
 #if __STDC_HOSTED__
